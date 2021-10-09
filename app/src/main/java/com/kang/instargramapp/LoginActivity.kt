b931e051aa1fc61fc0f7957f92d6e3ca1c89d22c
kangilbin <kib3388@naver.com>
2021-10-09 17:19:41 +0900

    구글 로그인  셋팅

--- app/src/main/java/com/kang/instargramapp/LoginActivity.kt
+++ app/src/main/java/com/kang/instargramapp/LoginActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
+    var goolgeSignInClient : GoogleSignInClient? = null
+    //구글 로그인 시 필요한 리퀘스트 코드
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -18,6 +28,16 @@ class LoginActivity : AppCompatActivity() {
         button1.setOnClickListener {
             signinAndSignup()
         }
+        button3.setOnClickListener {
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            //구글 api key 넣기
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail() //이메일 받아올 수 있게
+            .build()        // 다시 닫아 주기
+        //위에 설정한 옵션들을 클라이언트에 셋팅
+        goolgeSignInClient = GoogleSignIn.getClient(this,gso)
     }
 
     fun signinAndSignup() {
@@ -35,7 +55,37 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
+    //구글 로그인
+    fun googleLogin(){
+        var signInIntent = goolgeSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            // 구굴에서 넘겨주는 로그인 반환 값
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                // 성공했을 때 이 값을 Firebase에 넘겨준다.
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener  { task ->
+                if (task.isSuccessful) {
+                    // id와 paswword가 맞았을 때
+                    moveMainPage(task.result?.user)
+                } else {
+                    //틀릴 때
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
     //로그인 하는 코드
     fun signinEmail(){
         auth?.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
