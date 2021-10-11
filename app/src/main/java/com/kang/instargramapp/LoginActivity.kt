ab54607ecb8a309da96c340dba179aadf28be41e
kangilbin <kib3388@naver.com>
2021-10-12 00:09:06 +0900

    페이스북 로그인 완료

--- app/src/main/java/com/kang/instargramapp/LoginActivity.kt
+++ app/src/main/java/com/kang/instargramapp/LoginActivity.kt
@@ -8,25 +8,25 @@ import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
-import com.facebook.CallbackManager
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthCredential
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_login.*
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
     var googleSignInClient: GoogleSignInClient? = null
-    var callbackManager : CallbackManager? = null
+    var callbackManager: CallbackManager? = null
 
     //구글 로그인 시 필요한 리퀘스트 코드
     var GOOGLE_LOGIN_CODE = 9001
@@ -36,6 +36,9 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         button1.setOnClickListener {
             signinAndSignup()
+        }
+        button2.setOnClickListener {
+            facebookLogin()
         }
         button3.setOnClickListener {
             googleLogin()
@@ -47,6 +50,7 @@ class LoginActivity : AppCompatActivity() {
         //위에 설정한 옵션들을 클라이언트에 셋팅
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         //printHashKey()
+        callbackManager = CallbackManager.Factory.create()
     }
 
     fun signinAndSignup() {
@@ -64,8 +68,9 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
+
     //페이스북 로그인
-    fun printHashKey(){
+    fun printHashKey() {
         try {
             val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
             for (signature in info.signatures) {
@@ -74,10 +79,10 @@ class LoginActivity : AppCompatActivity() {
                 val hashKey = String(Base64.encode(md.digest(), 0))
                 Log.i("TAG", "key:$hashKey=")
             }
-        } catch (e: NoSuchAlgorithmException){
-            Log.e("TAG","printHashKey()", e)
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
         } catch (e: Exception) {
-            Log.e("TAG","printHashKey()", e)
+            Log.e("TAG", "printHashKey()", e)
         }
 
     }
@@ -88,8 +93,40 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
+    //페이스북 로그인
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+        //Result는 최종적으로 로그인을 설공했을 때 넘어오는 데이터
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    //페이스북 로그인을 성공하면 파이어베이스에 넘기는 코드
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+                override fun onCancel() {
+                }
+                override fun onError(error: FacebookException?) {
+                }
+            })
+    }
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // id와 paswword가 맞았을 때
+                    moveMainPage(task.result?.user)
+                } else {
+                    //틀릴 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GOOGLE_LOGIN_CODE) {
             // 구굴에서 넘겨주는 로그인 반환 값
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
