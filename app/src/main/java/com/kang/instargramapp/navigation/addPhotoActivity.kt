2fd4ad0b472393f0ca26866b379234600ed1e637
kangilbin <kib3388@naver.com>
2021-10-14 21:08:30 +0900

    사진 등록 뷰

---
+++ app/src/main/java/com/kang/instargramapp/navigation/addPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.kang.instargramapp.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+    }
+}
